e5e2bf40468c7a167cba10bb135766a90ec13031
KSangMin <fhfpszmfnwm@naver.com>
2025-02-21 19:22:35 +0900

    [Feat] 쿠키 오른쪽 이동 구현

--- Assets/Scripts/Entity/Cookie.cs
+++ Assets/Scripts/Entity/Cookie.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class Cookie : MonoBehaviour
 {
-    
+    Rigidbody2D _rb;
+
+    private int hp;
+    public int HP { get { return hp; } }
+    private float speed;
+    public float Speed { get { return speed; } }
+
+    private void Start()
+    {
+        hp = 162;
+        speed = 3;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 newPos = (Vector2)transform.position + new Vector2(Speed * Time.deltaTime, 0);
+        _rb.MovePosition(newPos);
+    }
 }
