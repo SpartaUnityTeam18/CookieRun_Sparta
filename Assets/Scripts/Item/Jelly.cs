0759f45fa808c99efb491a149d56d08223712872
EunJi <euuu0eeu@gmail.com>
2025-02-25 10:11:00 +0900

    [Fix] 모든 아이템 AddScore 및 태그 추가
    모든 아이템 획득 시 점수 추가적용 및
    Item 태그 적용

--- Assets/Scripts/Item/Jelly.cs
+++ Assets/Scripts/Item/Jelly.cs
@@ -7,10 +7,8 @@ using UnityEngine;
 /// </summary>
 public class Jelly : Item
 {
-    [SerializeField] private int addScore;
     public override void ApplyEffect(Cookie cookie)
     {
         SoundManager.Instance.PlaySFX("Jelly");
-        GameManager.Instance.AddScore(addScore);
     }
 }
